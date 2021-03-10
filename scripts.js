ad64dfb3043b096554b0a2790209f14622fb8af8
Patricia Bianca Lana Largura <patricia.largura@outlook.com>
2021-03-10 18:36:24 -0300

    Format Currency
    Created the function that get the result by Javascript

--- scripts.js
+++ scripts.js
@@ -55,3 +55,56 @@ const transactions = [
         }
 
 // I need to replace the data of the HTML by the JS data.
+
+const DOM = {
+    transactionsContainer: document.querySelector('#data-table tbody'),
+
+addTransaction(transaction, index){
+
+        const tr = document.createElement('tr')
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction) 
+
+        DOM.transactionsContainer.appendChild(tr)
+},
+
+        innerHTMLTransaction(transaction){
+            
+            const CSSclass = transaction.amount > 0 ? "income": "expense"
+
+            const amount = Utils.formatCurrency(transaction.amount)
+            
+            const html = 
+            ` 
+           
+            <td class="description">${transaction.description}</td>
+            <td class=${CSSclass}>${amount}</td>
+            <td>${transaction.date}</td>
+            <td>
+                <img src="./_assets/minus.svg" alt="Remover Transação">
+            </td>
+       
+        `
+        return html
+    }
+}
+
+const Utils = {
+    formatCurrency(value){
+        const signal = Number(value) < 0 ? "-"  :  ""
+
+        value  = String(value).replace(/\D/g, "")
+
+        value = Number(value) / 100
+        value = value.toLocaleString("pt-BR", {
+        style: "currency",
+        currency:"BRL"
+        })
+
+        return signal + value
+
+    }
+}
+
+transactions.forEach(function(transaction){
+    DOM.addTransaction(transaction)
+})
